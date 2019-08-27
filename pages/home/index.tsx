80d17d7a1fec24168981f91067bb9f41e653e5ce
AlbertPotter <smilepotter0128@gmail.com>
2019-08-27 13:56:39 +0300

    Remove callback from changeLanguage method

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -2,12 +2,18 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
+import * as i18next from 'i18next';
 //#endregion Global Imports
+
+//#region Local Imports
+import { Heading } from '@Components';
+import { HomeActions } from '@Actions';
+import { withI18next } from '../../src/withI18next';
+import './index.scss';
+//#region Local Imports
 
 //#region Interface Imports
 import { IHomePage, IStore } from '@Interfaces';
-import { Heading } from '@Components';
-import { HomeActions } from '@Actions';
 //#endregion Interface Imports
 
 export class HomePage extends React.Component<IHomePage.IProps, IHomePage.IState> {
@@ -15,13 +21,42 @@ export class HomePage extends React.Component<IHomePage.IProps, IHomePage.IState
 		super(props);
 	}
 
+	renderLocaleButtons = (activeLanguage: string) =>
+		['en', 'es', 'tr'].map(lang => (
+			<div
+				key={lang}
+				className={`button ${lang} ${activeLanguage === lang ? 'active' : ''}`}
+				onClick={() => this.changeLanguage(lang)}
+			>
+				{lang}
+			</div>
+		));
+
 	public render(): JSX.Element {
+		const { t, i18n } = this.props;
+
 		return (
-			<div className="title">
-				Hello!
-				<Heading text="World!" />
+			<div className="container">
+				<div className="container__top">
+					<img src="/static/images/pankod-logo.png" />
+				</div>
+				<div className="container__middle">
+					<div className="container__middle__left">
+						<div className="container__middle__left__buttons">
+							{this.renderLocaleButtons(i18n.language)}
+						</div>
+					</div>
+					<div className="container__middle__right">
+						<span className="container__top_text">{t('common:Hello')}</span>
+						<Heading text={t('common:World')} />
+					</div>
+				</div>
 			</div>
 		);
+	}
+
+	private changeLanguage(lang: string): void {
+		this.props.i18n.changeLanguage(lang);
 	}
 }
 
@@ -31,7 +66,9 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
 	Map: bindActionCreators(HomeActions.Map, dispatch),
 });
 
+const Extended = withI18next(['common'])(HomePage);
+
 export default connect(
 	mapStateToProps,
 	mapDispatchToProps,
-)(HomePage);
+)(Extended);
